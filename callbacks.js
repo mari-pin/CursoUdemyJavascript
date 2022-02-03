7ef54c6e9af03eb2b39e1ad7f16d08d238786fc3
Maria Ibañez <mirtraffik@gmail.com>
2022-02-03 11:35:31 +0100

    callback

--- callbacks.js
+++ callbacks.js
@@ -1,1 +1,21 @@
 //funciones k se pasan como paramatro a otrs funciones para que una vez finalizadas sus tereas ejecuten la funcion que hemos enviado como parametro.
+
+const suma = (a,b, callback)=>{
+    callback (a + b)
+}
+ suma (1, 2, (resultado)=>{
+     console.log(resultado);
+ })
+
+ const getData = (callback)=>{
+     setTimeout(() => {
+      callback ({
+        nombre:'maria',
+        apellido:'ibañez'
+        
+      })  
+        
+     }, 3000);
+ }
+ const imprimirData = (data)=> console.log(data);
+ getData(imprimirData);
