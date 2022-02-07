ed56ab48f9316cb6b8c7ef6f94beb0df09738352
Maria Ibañez <mirtraffik@gmail.com>
2022-02-07 13:26:06 +0100

    import api

--- ejercicioCliente-Servidor/index.js
+++ ejercicioCliente-Servidor/index.js
@@ -1,1 +1,18 @@
-const axios
+//traemos axios xa utilizarlo
+const axios = require ( 'axios');
+//axios trabaja de forma asincrona
+const main = async() =>{
+ //peticion get a la url y almacena la respuesta en la varible response
+let response = await axios.get('https://rickandmortyapi.com/api/character');
+let {data:{results}} = response;console.log(results);
+
+let personajes = results.map((personaje)=>{
+    return{
+        id:personaje.id,
+        nombre: personaje.name,
+        especie:personaje.species
+    }
+})
+console.log(personajes);
+}
+main()
