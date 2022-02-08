3c74520c39a5de2703c5d1bd65c995455d699779
Maria Ibañez <mirtraffik@gmail.com>
2022-02-08 11:19:27 +0100

    creacion ruta get, y puerto

--- servidoresWeb/index.js
+++ servidoresWeb/index.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000;
+//le damos la capacidad ewscuchar a nuestro sevidor peticiones tipo get 
+//request-->peticion del cliente
+//response--> respuesta al cliente
+app.get('/',(req, res)=>{
+    //responde datos. accedemos al json
+    res.json({
+        //estructuramos el objeto json que queremos responder
+        message:'hola desde expess'
+    })
+
+})
 
 app.listen(PORT, ()=>{
     console.log(`servidor escuchando en http://localhost:${PORT}`);
